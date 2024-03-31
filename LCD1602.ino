673f260756b50ed614ea40a111187729871e46ac
mbyakha <69594442+mbyakha@users.noreply.github.com>
2024-03-31 23:05:16 +0300

    Add files via upload

---
+++ LCD1602.ino
@@ -0,0 +1,25 @@
+//#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+	// initialize the LCD
+	lcd.begin();
+  
+	// Turn on the blacklight and print a message.
+	//lcd.backlight();
+	lcd.print("Welcome!");
+  delay(4000);
+}
+
+void loop()
+{
+	lcd.print("VIEWINCODE");
+ delay(1000);
+ lcd.clear(); //clear LCD screen
+ delay(1000);
+ 
+}
